fbff5e3e574ed56bf63435c50935c385e69c6a69
lpmayos <lpmayos@gmail.com>
2020-01-27 16:20:38 +0100

    added try/except to avoid FileNotFoundError

--- structural-probes/bert_base_cased_finetuned_squad_analyse_results.py
+++ structural-probes/bert_base_cased_finetuned_squad_analyse_results.py
@@ -18,38 +18,39 @@ def analyse_results(probe_name, models_path, runs_list, checkpoints_list, output
 
         for checkpoint in checkpoints_list:
 
-            checkpoint_path = run_path + '/results/checkpoint-' + checkpoint
-            squad_results_path = checkpoint_path + '/squad_results.json'
-            probes_path = checkpoint_path + '/structural_probes/' + probe_name
+            try:
+                checkpoint_path = run_path + '/results/checkpoint-' + checkpoint
+                squad_results_path = checkpoint_path + '/squad_results.json'
+                probes_path = checkpoint_path + '/structural_probes/' + probe_name
 
-            checkpoint_results = {'squad_exact_match': None,
-                                  'squad_f1': None,
-                                  'parse-depth': {},
-                                  'parse-distance': {}}
+                checkpoint_results = {'squad_exact_match': None,
+                                      'squad_f1': None,
+                                      'parse-depth': {},
+                                      'parse-distance': {}}
 
-            with open(squad_results_path) as file:
-                data = json.load(file)
-                checkpoint_results['squad_exact_match'] = data['squad_exact_match']
-                checkpoint_results['squad_f1'] = data['squad_f1']
+                with open(squad_results_path) as file:
+                    data = json.load(file)
+                    checkpoint_results['squad_exact_match'] = data['squad_exact_match']
+                    checkpoint_results['squad_f1'] = data['squad_f1']
 
-            with open(probes_path + '/parse-depth/dev.root_acc') as file:
-                result = file.readlines()[0].split()[0]  # i.e. 0.8123529411764706      1381    1700
-                checkpoint_results['parse-depth']['dev.root_acc'] = result
-            with open(probes_path + '/parse-depth/dev.spearmanr-5_50-mean') as file:
-                result = file.readlines()[0]  # i.e. 0.8552411954331226
-                checkpoint_results['parse-depth']['dev.spearmanr-5_50-mean'] = result
+                with open(probes_path + '/parse-depth/dev.root_acc') as file:
+                    result = file.readlines()[0].split()[0]  # i.e. 0.8123529411764706      1381    1700
+                    checkpoint_results['parse-depth']['dev.root_acc'] = result
+                with open(probes_path + '/parse-depth/dev.spearmanr-5_50-mean') as file:
+                    result = file.readlines()[0]  # i.e. 0.8552411954331226
+                    checkpoint_results['parse-depth']['dev.spearmanr-5_50-mean'] = result
 
-            with open(probes_path + '/parse-distance/dev.uuas') as file:
-                result = file.readlines()[0]  # i.e. 0.7040907201804905
-                checkpoint_results['parse-distance']['dev.uuas'] = result
-            with open(probes_path + '/parse-distance/dev.spearmanr-5_50-mean') as file:
-                result = file.readlines()[0]  # i.e. 0.8058858201615192
-                checkpoint_results['parse-distance']['dev.spearmanr-5_50-mean'] = result
+                with open(probes_path + '/parse-distance/dev.uuas') as file:
+                    result = file.readlines()[0]  # i.e. 0.7040907201804905
+                    checkpoint_results['parse-distance']['dev.uuas'] = result
+                with open(probes_path + '/parse-distance/dev.spearmanr-5_50-mean') as file:
+                    result = file.readlines()[0]  # i.e. 0.8058858201615192
+                    checkpoint_results['parse-distance']['dev.spearmanr-5_50-mean'] = result
 
-            results[run]['checkpoint-' + checkpoint] = checkpoint_results
+                results[run]['checkpoint-' + checkpoint] = checkpoint_results
 
-            # except:
-            #     logging.info('ATTENTION! Could not process results for %s' % checkpoint_path)
+            except FileNotFoundError:
+                logging.info('ATTENTION! Could not process results for %s' % checkpoint_path)
 
     with open(output_file, 'w') as outfile:
         json.dump(results, outfile)
